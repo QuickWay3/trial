a0ae3eab29a1ae16dd01a545defd3eb38f4ae83c
QuickWay3 <quickway32@gmail.com>
2024-03-08 21:34:01 +0530

    trial folder comitted

---
+++ print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class print{
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hey !!! this is me java");
+        sc.close();
+    }
+}
